6a2941c7b194a8f7ca4a5318ab15ed9436e82bc4
Viniciusx0800 <vinioliveira2008y@gmail.com>
2024-11-01 10:42:49 -0300

    não sei o que aconteceu aqui

--- NewTalentsTest/obj/Debug/net8.0/NewTalentsTest.AssemblyInfo.cs
+++ NewTalentsTest/obj/Debug/net8.0/NewTalentsTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewTalentsTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8559ba985319c3c131d63cae42b90edeec90824b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bda14cf89e1c36b6b754505943af53afd5ef8be")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewTalentsTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewTalentsTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
